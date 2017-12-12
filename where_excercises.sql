fadfd84ac183751d3b092bc635a32a7988b0e825
Mary Kathryn Syms <baglady@Marys-MacBook-Pro.local>
2017-12-12 11:25:39 -0600

    finished MySQL Clauses - where excercise

---
+++ where_excercises.sql
@@ -0,0 +1,59 @@
+USE employees_db;
+
+SELECT *
+FROM employees
+WHERE first_name IN ('Irena', 'Vidya', 'Maya');
+
+SELECT *
+FROM employees
+WHERE last_name LIKE ('E%');
+
+SELECT *
+FROM employees
+WHERE hire_date >= '1990-01-01' AND hire_date <= '1999-12-31';
+
+SELECT *
+FROM employees
+WHERE hire_date BETWEEN '1990-01-01' AND '1999-12-31';
+
+SELECT *
+FROM employees
+WHERE hire_date LIKE '199%';
+
+SELECT *
+FROM employees
+WHERE birth_date LIKE '%-12-25%';
+
+SELECT *
+FROM employees
+WHERE last_name LIKE '%q%';
+
+##########################################################################
+
+SELECT *
+FROM employees
+WHERE first_name = 'Irena' OR first_name = 'Vidya' OR first_name = 'Maya';
+
+SELECT *
+FROM employees
+WHERE gender = 'M'
+      AND (
+        first_name = 'Irena'
+        OR first_name = 'Vidya'
+        OR first_name = 'Maya');
+
+SELECT *
+FROM employees
+WHERE last_name LIKE ('%E%');
+
+SELECT *
+FROM employees
+WHERE last_name LIKE '%E' AND last_name LIKE 'E%';
+
+SELECT *
+FROM employees
+WHERE hire_date LIKE '199%' AND birth_date LIKE '%-12-25%';
+
+SELECT *
+FROM employees
+WHERE last_name LIKE '%Q%' AND last_name NOT LIKE '%QU%';
