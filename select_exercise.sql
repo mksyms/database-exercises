d0583902388940cbb147a9d9851451ce5b28daf9
Mary Kathryn Syms <baglady@Marys-MacBook-Pro.local>
2017-12-12 09:30:28 -0600

    finished select exercise

--- select_exercise.sql
+++ select_exercise.sql
@@ -18,3 +18,23 @@ SELECT release_date
 FROM albums
 WHERE name = 'Sgt. Pepper''s Lonely Hearts';
 
+SELECT 'The genre for Nevermind' AS '';
+SELECT genre
+FROM albums
+WHERE name = 'Nevermind';
+
+SELECT 'Which albums were released in the 1990s' AS '';
+SELECT *
+FROM albums
+WHERE release_date BETWEEN 1990 AND 1999;
+
+
+SELECT 'Which albums had less than 20 million certified sales' AS '';
+SELECT *
+FROM albums
+WHERE sales < 20;
+
+SELECT 'All the albums with a genre of "Rock"' AS '';
+SELECT *
+FROM albums
+WHERE genre LIKE '%rock%';
