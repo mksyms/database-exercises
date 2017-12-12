718b1a8caf7b862a6cd71b3d572f429458038fa3
Mary Kathryn Syms <baglady@Marys-MacBook-Pro.local>
2017-12-12 12:15:17 -0600

    Finished MySQL Clauses Order Exercise

--- order_by_exercises.sql
+++ order_by_exercises.sql
@@ -2,4 +2,35 @@ USE employees_db;
 
 SELECT *
 FROM employees
-WHERE first_name IN ('Irena', 'Vidya', 'Maya');
+WHERE first_name IN ('Irena', 'Vidya', 'Maya')
+ORDER BY first_name;
+
+SELECT *
+FROM employees
+WHERE first_name IN ('Irena', 'Vidya', 'Maya')
+ORDER BY first_name, last_name ASC;
+
+SELECT *
+FROM employees
+WHERE first_name IN ('Irena', 'Vidya', 'Maya')
+ORDER BY last_name ASC, first_name ASC;
+
+SELECT *
+FROM employees
+WHERE last_name LIKE ('%E%')
+ORDER BY emp_no ASC;
+
+SELECT *
+FROM employees
+WHERE first_name IN ('Irena', 'Vidya', 'Maya')
+ORDER BY last_name DESC, first_name DESC;
+
+SELECT *
+FROM employees
+WHERE last_name LIKE ('%E%')
+ORDER BY emp_no DESC;
+
+SELECT *
+FROM employees
+WHERE hire_date LIKE '199%' AND birth_date LIKE '%-12-25%'
+ORDER BY birth_date ASC, hire_date DESC;
