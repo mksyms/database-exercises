142e51750e04ef55df780e4d2029078833098a24
Mary Kathryn Syms <baglady@Marys-MacBook-Pro.local>
2017-12-13 10:15:03 -0600

    cleaned up code for functions exercise database return set

--- functions_exercises.sql
+++ functions_exercises.sql
@@ -5,7 +5,7 @@ FROM employees
 WHERE last_name LIKE ('%E%')
 ORDER BY emp_no ASC;
 
-SELECT concat('Employee has worked here',DATEDIFF(NOW(), hire_date),'days')
+SELECT concat(DATEDIFF(NOW(), hire_date)) AS 'Days Worked', first_name, last_name
 FROM employees
 WHERE hire_date LIKE '199%' AND birth_date LIKE '%-12-25%'
 ORDER BY birth_date ASC, hire_date DESC;
